338dc3b73107392e3e0dadfb82d170c088bfc90c
Владимир Походня <pokhodnya-ngs.ru@yandex.ru>
2024-12-16 03:27:55 +0700

    Added http logging

---
+++ src/main/java/com/github/vladimirpokhodnya/aophttploggingstarter/config/HttpLoggingAutoConfiguration.java
@@ -0,0 +1,20 @@
+package com.github.vladimirpokhodnya.aophttploggingstarter.config;
+
+import com.github.vladimirpokhodnya.aophttploggingstarter.aspect.HttpLoggingAspect;
+import com.github.vladimirpokhodnya.aophttploggingstarter.service.HttpRequestService;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+@AutoConfiguration
+@EnableConfigurationProperties(HttpLoggingProperties.class)
+public class HttpLoggingAutoConfiguration {
+    @Bean
+    @ConditionalOnProperty(name = "http.logging.enabled", havingValue = "true", matchIfMissing = true)
+    public HttpLoggingAspect httpLoggingAspect(HttpLoggingProperties properties,
+                                               HttpRequestService requestService) {
+        return new HttpLoggingAspect(properties, requestService);
+    }
+
+}
---
+++ src/main/java/com/github/vladimirpokhodnya/aophttploggingstarter/config/HttpLoggingProperties.java
@@ -0,0 +1,31 @@
+package com.github.vladimirpokhodnya.aophttploggingstarter.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "http.logging")
+public class HttpLoggingProperties {
+    private boolean enabled = true;
+    private LogLevel level = LogLevel.MEDIUM;
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public LogLevel getLevel() {
+        return level;
+    }
+
+    public void setLevel(LogLevel level) {
+        this.level = level;
+    }
+
+    public enum LogLevel {
+        FULL,
+        MEDIUM,
+        MINIMAL
+    }
+}
