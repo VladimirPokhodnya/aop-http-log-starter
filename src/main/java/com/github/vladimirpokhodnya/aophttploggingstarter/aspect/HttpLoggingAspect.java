90c8c8abd5b5c2764af91ef4de8f30324d618088
Владимир Походня <pokhodnya-ngs.ru@yandex.ru>
2024-12-17 04:01:40 +0700

    Refactoring HttpLoggingAspect

--- src/main/java/com/github/vladimirpokhodnya/aophttploggingstarter/aspect/HttpLoggingAspect.java
+++ src/main/java/com/github/vladimirpokhodnya/aophttploggingstarter/aspect/HttpLoggingAspect.java
@@ -2,6 +2,7 @@ package com.github.vladimirpokhodnya.aophttploggingstarter.aspect;
 
 import com.github.vladimirpokhodnya.aophttploggingstarter.config.HttpLoggingProperties;
 import com.github.vladimirpokhodnya.aophttploggingstarter.service.HttpRequestService;
+import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.aspectj.lang.ProceedingJoinPoint;
@@ -15,6 +16,11 @@ public class HttpLoggingAspect {
 private static final Logger logger = LogManager.getLogger(HttpLoggingAspect.class);
 
     private final HttpLoggingProperties properties;
+
+    public static final Level MINIMAL = Level.forName("MINIMAL HTTP LOG", 350);
+    public static final Level MEDIUM = Level.forName("MEDIUM HTTP LOG", 351);
+    public static final Level FULL = Level.forName("FULL HTTP LOG", 352);
+
     private final HttpRequestService requestService;
 
     public HttpLoggingAspect(HttpLoggingProperties properties, HttpRequestService requestService) {
@@ -41,21 +47,21 @@ private static final Logger logger = LogManager.getLogger(HttpLoggingAspect.clas
     }
 
     private void logIncomingRequestHeaders() {
-        logger.log(CustomLogLevels.MEDIUM, "Incoming headers: {}", requestService.getRequestHeaders());
+        logger.log(MEDIUM, "Incoming headers: {}", requestService.getRequestHeaders());
     }
 
     private void logIncomingRequestInfo() {
-        logger.log(CustomLogLevels.MINIMAL,"Incoming request: method={}, URL={}", requestService.getMethod(), requestService.getRequestURL());
+        logger.log(MINIMAL,"Incoming request: method={}, URL={}", requestService.getMethod(), requestService.getRequestURL());
     }
 
     private void logOutgoingResponse(Object response) {
-        logger.log(CustomLogLevels.MINIMAL,"Outgoing response: {}", response);
+        logger.log(MINIMAL,"Outgoing response: {}", response);
     }
 
     private void logRequestBody(ProceedingJoinPoint joinPoint) {
         Object[] args = joinPoint.getArgs();
         String requestBody = requestService.getRequestBody(args);
-        logger.log(CustomLogLevels.FULL,"Incoming request body: {}", requestBody);
+        logger.log(FULL,"Incoming request body: {}", requestBody);
     }
 }
 
